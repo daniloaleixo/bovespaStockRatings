45dc67a0db6665ec5fa90b26ee647096381c267d
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-04-20 17:50:05 -0300

    Acoes estoa sendo mostradas corretamente com todas as infos

--- getFromFirebase.js
+++ getFromFirebase.js
@@ -53,35 +53,44 @@ var buildTable = function(data){
 	Object.keys(data).forEach(function(key){
 		if(parseInt(key) < 1000){
 			Object.keys(data[key]).forEach(function(stock){
-				var stockInfo = {
-					stockName: stock,
-					nota: data[key][stock]["nota"],
-					cotacao: data[key][stock]["cotacao"],
-					patLiq: data[key][stock]["Pat.Liq"],
-					LiqCorr: data[key][stock]["Liq.Corr."],
-					ROE: data[key][stock]["ROE"],
-					DivPat: data[key][stock]["Div.Brut/Pat."],
-					Cresc5: data[key][stock]["Cresc.5a"],
-					PVP: data[key][stock]["P/VP"],
-					PL: data[key][stock]["P/L"],
-					Div: data[key][stock]["DY"]
-				}
+				// var stockInfo = {
+				// 	stockName: stock,
+				// 	nota: data[key][stock]["nota"],
+				// 	cotacao: data[key][stock]["cotacao"],
+				// 	patLiq: data[key][stock]["Pat.Liq"],
+				// 	LiqCorr: data[key][stock]["Liq.Corr."],
+				// 	ROE: data[key][stock]["ROE"],
+				// 	DivPat: data[key][stock]["Div.Brut/Pat."],
+				// 	Cresc5: data[key][stock]["Cresc.5a"],
+				// 	PVP: data[key][stock]["P/VP"],
+				// 	PL: data[key][stock]["P/L"],
+				// 	Div: data[key][stock]["DY"]
+				// }
 				// console.log(stockInfo);
 
 				var stockLine = "<tr>" + 
-								"<td class='stock'>" + stockInfo.stockName + "</td>" + 
-				            	"<td>" + stockInfo.nota + "</td>" + 
-				            	"<td>" + stockInfo.cotacao + "</td>" + 
-				            	"<td>" + stockInfo.patLiq + "</td>" + 
-				            	"<td>" + stockInfo.LiqCorr + "</td>" + 
-				            	"<td>" + stockInfo.ROE + "</td>" + 
-				            	"<td>" + stockInfo.DivPat + "</td>" + 
-				            	"<td>" + stockInfo.Cresc5 + "</td>" + 
-				            	"<td>" + stockInfo.PVP + "</td>" + 
-				            	"<td>" + stockInfo.PL + "</td>" + 
-				            	"<td>" + stockInfo.Div + "</td>" + 
+								"<td class='stock'>" + stock + "</td>" + 
+				            	"<td>" + data[key][stock]["nota"] + "</td>" +
+				            	"<td>" + data[key][stock]["cotacao"] + "</td>" +
+				            	"<td>" + data[key][stock]["Pat.Liq"] + "</td>" +
+				            	"<td>" + data[key][stock]["Liq.Corr."] + "</td>" +
+				            	"<td>" + data[key][stock]["ROE"] + "</td>" +
+				            	"<td>" + data[key][stock]["Div.Brut/Pat."] + "</td>" +
+				            	"<td>" + data[key][stock]["Cresc.5a"] + "</td>" +
+				            	"<td>" + data[key][stock]["P/VP"] + "</td>" +
+				            	"<td>" + data[key][stock]["P/L"] + "</td>" +
+				            	"<td>" + data[key][stock]["DY"] + "</td>" +
+				            	"<td>" + data[key][stock]["PSR"] + "</td>" +
+				            	"<td>" + data[key][stock]["P/Ativo"] + "</td>" +
+				            	"<td>" + data[key][stock]["P/Cap.Giro"] + "</td>" +
+				            	"<td>" + data[key][stock]["P/EBIT"] + "</td>" +
+				            	"<td>" + data[key][stock]["P/Ativ.Circ.Liq."] + "</td>" +
+				            	"<td>" + data[key][stock]["EV/EBIT"] + "</td>" +
+				            	"<td>" + data[key][stock]["EBITDA"] + "</td>" +
+				            	"<td>" + data[key][stock]["Mrg.Liq."] + "</td>" +
+				            	"<td>" + data[key][stock]["ROIC"] + "</td>" +
+				            	"<td>" + data[key][stock]["Liq.2m."] + "</td>" +
 				        		"</tr>";
-				// console.log(stockLine);
 
 				txt += stockLine;
 			});
