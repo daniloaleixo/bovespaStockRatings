cb37a8a7f8bb77a8f4d4720ea1ad5967b32a3986
Danilo Aleixo <danilo_aleixo@hotmail.com>
2019-06-24 23:58:03 -0300

    Versão 1 funcionando
    - Estou conseguindo pegar as leituras da funcao

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,16 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+export const saveStockHistory = functions.database.ref('/stocks')
+    .onWrite((change, context) => {
+      // Grab the current value of what was written to the Realtime Database.
+      const original = change;
+      return ['Uppercasing', context, original];
+      // const uppercase = original.toUpperCase();
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to the Firebase Realtime Database.
+      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+      // return snapshot.ref.parent.child('uppercase').set(uppercase);
+    });
