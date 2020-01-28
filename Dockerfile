2d579ddd711f2a1f432a51d385477de2ef612fd8
Danilo Aleixo <danilo_aleixo@hotmail.com>
2020-01-27 23:42:10 -0300

    Mandnado email para saber se da certo ou não
    issue #18

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,7 @@ RUN pip install --upgrade lxml && \
     pip install --upgrade pycryptodome==3.4.3 && \
     pip install --upgrade requests_toolbelt && \
     pip install --upgrade pymongo && \
+    pip install sendgrid && \
     python -m pip install pymongo[srv]
 
 CMD python3 fundamentus.py
--- fundamentus.py
+++ fundamentus.py
@@ -14,6 +14,8 @@ import ast
 import datetime
 import os
 from pymongo import MongoClient
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 
 def get_data(*args, **kwargs):
@@ -181,165 +183,187 @@ def get_specific_data(stock):
 
 
 
-
-
-
-
-    
-if __name__ == '__main__':
-    from waitingbar import WaitingBar
-    
-    THE_BAR = WaitingBar('[*] Downloading...')
-
-    lista = get_data()
-    THE_BAR.stop()
-    # print("Get all stocks data")
-
-
-    #Transform em uma lista, agora preciso passar para formato JSON
-    array_format = list(lista.items())
-
-    # print(array_format, len(array_format))
-
-    hashes_list = []
-    # First include the list of all hashes
-    for i in range(0, len(array_format)):
-        hashes_list.append(array_format[i][1])
-
-    stocks = []
-    # Then from a list of hashes we will transform to a list of stocks
-    for i in range(0, len(hashes_list)):
-        for key in hashes_list[i]:
-            # Adds stockCode
-            hashes_list[i][key]["stockCode"] = key
-            stocks.append(hashes_list[i][key])
+def send_email(message):
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+    except Exception as e:
+        print("Deu erro na hora de enviar o email", str(e))
     
 
 
-#     json_format = {
-#     "0": {
-#         "DAGB33": {
-#             "Cresc.5a": "46,43%",
-#             "DY": "0,00%",
-#             "Div.Brut/Pat.": "1,37",
-#             "EBITDA": "4,75%",
-#             "EV/EBIT": "0,00",
-#             "Liq.2m.": "916.730,00",
-#             "Liq.Corr.": "1,16",
-#             "Mrg.Liq.": "0,38%",
-#             "P/Ativ.Circ.Liq.": "0,00",
-#             "P/Ativo": "0,000",
-#             "P/Cap.Giro": "0,00",
-#             "P/EBIT": "0,00",
-#             "P/L": "0,00",
-#             "P/VP": "0,00",
-#             "PSR": "0,000",
-#             "Pat.Liq": "9.803.230.000,00",
-#             "ROE": "-0,47%",
-#             "ROIC": "4,59%",
-#             "cotacao": "480,00",
-#             "nota": 0.5
-#         }
-#     }
-# }
-
-    # Calculate the score of the stock
-    final_stocks = []
-    for stock in stocks:
-        nota = 0
-        patrLiq = float(stock["Pat.Liq"].replace('.', '').replace(',', '.'))
-        if patrLiq > 2000000000:
-            nota = nota + 1
-        liqCorr = float(stock["Liq.Corr."].replace('.', '').replace(',', '.').replace('%', ''))
-        if liqCorr > 1.5:
-            nota = nota + 1
-        roe = float(stock["ROE"].replace('.', '').replace(',', '.').replace('%', ''))
-        if roe > 20: 
-            nota = nota + 1
-        divPat = float(stock["Div.Brut/Pat."].replace('.', '').replace(',', '.').replace('%', ''))
-        if divPat < 0.5 and divPat > 0: 
-            nota = nota + 1
-        cresc = float(stock["Cresc.5a"].replace('.', '').replace(',', '.').replace('%', ''))
-        if cresc > 5: 
-            nota = nota + 1
-        pvp = float(stock["P/VP"].replace('.', '').replace(',', '.').replace('%', ''))
-        if pvp < 2 and pvp > 0: 
-            nota = nota + 1
-        pl = float(stock["P/L"].replace('.', '').replace(',', '.').replace('%', ''))
-        if pl < 15 and pl > 0: 
-            nota = nota + 1
-        dy = float(stock["DY"].replace('.', '').replace(',', '.').replace('%', ''))
-        if dy > 2.5: 
-            nota = nota + 1
-        stock["nota"] = float(nota) / 8.0 * 10.0
-
-        newStock = {}
-        newStock["stockCode"] = stock["stockCode"]
-        newStock["patrimonioLiquido"] = patrLiq
-        newStock["liquidezCorrente"] = liqCorr
-        newStock["ROE"] = roe
-        newStock["divSobrePatrimonio"] = divPat
-        newStock["crescimentoCincoAnos"] = cresc
-        newStock["precoSobreVP"] = pvp
-        newStock["precoSobreLucro"] = pl
-        newStock["dividendos"] = dy
-        newStock["score"] = stock["nota"]
-        newStock["stockPrice"] = float(stock["cotacao"].replace('.', '').replace(',', '.'))
-        newStock["PSR"] = float(stock["PSR"].replace('.', '').replace(',', '.'))
-        newStock["precoSobreAtivo"] = float(stock['P/Ativo'].replace('.', '').replace(',', '.'))
-        newStock["precoSobreCapitalGiro"] = float(stock['P/Cap.Giro'].replace('.', '').replace(',', '.'))
-        newStock["precoSobreEBIT"] = float(stock['P/EBIT'].replace('.', '').replace(',', '.'))
-        newStock["precoSobreAtivoCirculante"] = float(stock['P/Ativ.Circ.Liq.'].replace('.', '').replace(',', '.'))
-        newStock["EVSobreEBIT"] = float(stock['EV/EBIT'].replace('.', '').replace(',', '.'))
-        newStock["margemEBIT"] = float(stock["EBITDA"].replace('.', '').replace(',', '.').replace('%', ''))
-        newStock["margemLiquida"] = float(stock['Mrg.Liq.'].replace('.', '').replace(',', '.').replace('%', ''))
-        newStock["ROIC"] = float(stock["ROIC"].replace('.', '').replace(',', '.').replace('%', ''))
-        newStock["liquidezDoisMeses"] = float(stock['Liq.2m.'].replace('.', '').replace(',', '.').replace('%', ''))
-        newStock["timestamp"] = datetime.datetime.now()
-
-        # Get more information
-        print("Getting more information from stock ", newStock["stockCode"])
-        specific_data = get_specific_data(newStock["stockCode"])
-
-
-        # Add everything to the object
-        newStock["tipo"] = specific_data['Tipo']
-        newStock["name"] = specific_data['Empresa']
-        newStock["setor"] = specific_data['Setor']
-        newStock["subsetor"] = specific_data['Subsetor']
-        newStock["max52sem"] = float(specific_data['Max 52 sem'].replace('.', '').replace(',', '.')) if 'Max 52 sem' in specific_data and not "-" in specific_data['Max 52 sem'] else 0
-        newStock["volMed2M"] = float(specific_data['Vol $ méd (2m)'].replace('.', '').replace(',', '.')) if 'Vol $ méd (2m)' in specific_data and not "-" in specific_data['Vol $ méd (2m)'] else 0
-        newStock["valorMercado"] = float(specific_data['Valor de mercado'].replace('.', '').replace(',', '.')) if 'Valor de mercado' in specific_data and not "-" in specific_data['Valor de mercado'] else 0
-        newStock["valorFirma"] = float(specific_data['Valor da firma'].replace('.', '').replace(',', '.')) if 'Valor da firma' in specific_data and not "-" in specific_data['Valor da firma'] else 0
-        newStock["nAcoes"] = float(specific_data['Nro. Ações'].replace('.', '').replace(',', '.')) if 'Nro. Ações' in specific_data and not "-" in specific_data['Nro. Ações'] else 0
-        newStock["lucroPorAcao"] = float(specific_data['LPA'].replace('.', '').replace(',', '.')) if 'LPA' in specific_data and not "-" in specific_data['LPA'] else 0
-        newStock["margemBruta"] = float(specific_data['Marg. Bruta'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Marg. Bruta' in specific_data and not "-" in specific_data['Marg. Bruta'] else 0
-        newStock["EBITsobreAtivo"] = float(specific_data['EBIT / Ativo'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'EBIT / Ativo' in specific_data and not "-" in specific_data['EBIT / Ativo'] else 0
-        newStock["giroAtivos"] = float(specific_data['Giro Ativos'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Giro Ativos' in specific_data and not "-" in specific_data['Giro Ativos'] else 0
-        newStock["ativo"] = float(specific_data['Ativo'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Ativo' in specific_data and not "-" in specific_data['Ativo'] else 0
-        newStock["divBruta"] = float(specific_data['Dív. Bruta'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Dív. Bruta' in specific_data and not "-" in specific_data['Dív. Bruta'] else 0
-        newStock["divLiquida"] = float(specific_data['Dív. Líquida'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Dív. Líquida' in specific_data and not "-" in specific_data['Dív. Líquida'] else 0
-        newStock["disponibilidades"] = float(specific_data['Disponibilidades'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Disponibilidades' in specific_data and not "-" in specific_data['Disponibilidades'] else 0
-        newStock["receitaLiquida"] = float(specific_data['Receita Líquida'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Receita Líquida' in specific_data and not "-" in specific_data['Receita Líquida'] else 0
-        newStock["lucroLiquido"] = float(specific_data['Lucro Líquido'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Lucro Líquido' in specific_data and not "-" in specific_data['Lucro Líquido'] else 0
-
+try: 
+    if __name__ == '__main__':
+        from waitingbar import WaitingBar
         
-        final_stocks.append(newStock)
+        THE_BAR = WaitingBar('[*] Downloading...')
+
+        lista = get_data()
+        THE_BAR.stop()
+        # print("Get all stocks data")
+
+
+        #Transform em uma lista, agora preciso passar para formato JSON
+        array_format = list(lista.items())
+
+        # print(array_format, len(array_format))
+
+        hashes_list = []
+        # First include the list of all hashes
+        for i in range(0, len(array_format)):
+            hashes_list.append(array_format[i][1])
+
+        stocks = []
+        # Then from a list of hashes we will transform to a list of stocks
+        for i in range(0, len(hashes_list)):
+            for key in hashes_list[i]:
+                # Adds stockCode
+                hashes_list[i][key]["stockCode"] = key
+                stocks.append(hashes_list[i][key])
+        
+
+
+    #     json_format = {
+    #     "0": {
+    #         "DAGB33": {
+    #             "Cresc.5a": "46,43%",
+    #             "DY": "0,00%",
+    #             "Div.Brut/Pat.": "1,37",
+    #             "EBITDA": "4,75%",
+    #             "EV/EBIT": "0,00",
+    #             "Liq.2m.": "916.730,00",
+    #             "Liq.Corr.": "1,16",
+    #             "Mrg.Liq.": "0,38%",
+    #             "P/Ativ.Circ.Liq.": "0,00",
+    #             "P/Ativo": "0,000",
+    #             "P/Cap.Giro": "0,00",
+    #             "P/EBIT": "0,00",
+    #             "P/L": "0,00",
+    #             "P/VP": "0,00",
+    #             "PSR": "0,000",
+    #             "Pat.Liq": "9.803.230.000,00",
+    #             "ROE": "-0,47%",
+    #             "ROIC": "4,59%",
+    #             "cotacao": "480,00",
+    #             "nota": 0.5
+    #         }
+    #     }
+    # }
+
+        # Calculate the score of the stock
+        final_stocks = []
+        for stock in stocks:
+            nota = 0
+            patrLiq = float(stock["Pat.Liq"].replace('.', '').replace(',', '.'))
+            if patrLiq > 2000000000:
+                nota = nota + 1
+            liqCorr = float(stock["Liq.Corr."].replace('.', '').replace(',', '.').replace('%', ''))
+            if liqCorr > 1.5:
+                nota = nota + 1
+            roe = float(stock["ROE"].replace('.', '').replace(',', '.').replace('%', ''))
+            if roe > 20: 
+                nota = nota + 1
+            divPat = float(stock["Div.Brut/Pat."].replace('.', '').replace(',', '.').replace('%', ''))
+            if divPat < 0.5 and divPat > 0: 
+                nota = nota + 1
+            cresc = float(stock["Cresc.5a"].replace('.', '').replace(',', '.').replace('%', ''))
+            if cresc > 5: 
+                nota = nota + 1
+            pvp = float(stock["P/VP"].replace('.', '').replace(',', '.').replace('%', ''))
+            if pvp < 2 and pvp > 0: 
+                nota = nota + 1
+            pl = float(stock["P/L"].replace('.', '').replace(',', '.').replace('%', ''))
+            if pl < 15 and pl > 0: 
+                nota = nota + 1
+            dy = float(stock["DY"].replace('.', '').replace(',', '.').replace('%', ''))
+            if dy > 2.5: 
+                nota = nota + 1
+            stock["nota"] = float(nota) / 8.0 * 10.0
+
+            newStock = {}
+            newStock["stockCode"] = stock["stockCode"]
+            newStock["patrimonioLiquido"] = patrLiq
+            newStock["liquidezCorrente"] = liqCorr
+            newStock["ROE"] = roe
+            newStock["divSobrePatrimonio"] = divPat
+            newStock["crescimentoCincoAnos"] = cresc
+            newStock["precoSobreVP"] = pvp
+            newStock["precoSobreLucro"] = pl
+            newStock["dividendos"] = dy
+            newStock["score"] = stock["nota"]
+            newStock["stockPrice"] = float(stock["cotacao"].replace('.', '').replace(',', '.'))
+            newStock["PSR"] = float(stock["PSR"].replace('.', '').replace(',', '.'))
+            newStock["precoSobreAtivo"] = float(stock['P/Ativo'].replace('.', '').replace(',', '.'))
+            newStock["precoSobreCapitalGiro"] = float(stock['P/Cap.Giro'].replace('.', '').replace(',', '.'))
+            newStock["precoSobreEBIT"] = float(stock['P/EBIT'].replace('.', '').replace(',', '.'))
+            newStock["precoSobreAtivoCirculante"] = float(stock['P/Ativ.Circ.Liq.'].replace('.', '').replace(',', '.'))
+            newStock["EVSobreEBIT"] = float(stock['EV/EBIT'].replace('.', '').replace(',', '.'))
+            newStock["margemEBIT"] = float(stock["EBITDA"].replace('.', '').replace(',', '.').replace('%', ''))
+            newStock["margemLiquida"] = float(stock['Mrg.Liq.'].replace('.', '').replace(',', '.').replace('%', ''))
+            newStock["ROIC"] = float(stock["ROIC"].replace('.', '').replace(',', '.').replace('%', ''))
+            newStock["liquidezDoisMeses"] = float(stock['Liq.2m.'].replace('.', '').replace(',', '.').replace('%', ''))
+            newStock["timestamp"] = datetime.datetime.now()
+
+            # Get more information
+            print("Getting more information from stock ", newStock["stockCode"])
+            specific_data = get_specific_data(newStock["stockCode"])
+
+
+            # Add everything to the object
+            newStock["tipo"] = specific_data['Tipo']
+            newStock["name"] = specific_data['Empresa']
+            newStock["setor"] = specific_data['Setor']
+            newStock["subsetor"] = specific_data['Subsetor']
+            newStock["max52sem"] = float(specific_data['Max 52 sem'].replace('.', '').replace(',', '.')) if 'Max 52 sem' in specific_data and not "-" in specific_data['Max 52 sem'] else 0
+            newStock["volMed2M"] = float(specific_data['Vol $ méd (2m)'].replace('.', '').replace(',', '.')) if 'Vol $ méd (2m)' in specific_data and not "-" in specific_data['Vol $ méd (2m)'] else 0
+            newStock["valorMercado"] = float(specific_data['Valor de mercado'].replace('.', '').replace(',', '.')) if 'Valor de mercado' in specific_data and not "-" in specific_data['Valor de mercado'] else 0
+            newStock["valorFirma"] = float(specific_data['Valor da firma'].replace('.', '').replace(',', '.')) if 'Valor da firma' in specific_data and not "-" in specific_data['Valor da firma'] else 0
+            newStock["nAcoes"] = float(specific_data['Nro. Ações'].replace('.', '').replace(',', '.')) if 'Nro. Ações' in specific_data and not "-" in specific_data['Nro. Ações'] else 0
+            newStock["lucroPorAcao"] = float(specific_data['LPA'].replace('.', '').replace(',', '.')) if 'LPA' in specific_data and not "-" in specific_data['LPA'] else 0
+            newStock["margemBruta"] = float(specific_data['Marg. Bruta'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Marg. Bruta' in specific_data and not "-" in specific_data['Marg. Bruta'] else 0
+            newStock["EBITsobreAtivo"] = float(specific_data['EBIT / Ativo'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'EBIT / Ativo' in specific_data and not "-" in specific_data['EBIT / Ativo'] else 0
+            newStock["giroAtivos"] = float(specific_data['Giro Ativos'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Giro Ativos' in specific_data and not "-" in specific_data['Giro Ativos'] else 0
+            newStock["ativo"] = float(specific_data['Ativo'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Ativo' in specific_data and not "-" in specific_data['Ativo'] else 0
+            newStock["divBruta"] = float(specific_data['Dív. Bruta'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Dív. Bruta' in specific_data and not "-" in specific_data['Dív. Bruta'] else 0
+            newStock["divLiquida"] = float(specific_data['Dív. Líquida'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Dív. Líquida' in specific_data and not "-" in specific_data['Dív. Líquida'] else 0
+            newStock["disponibilidades"] = float(specific_data['Disponibilidades'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Disponibilidades' in specific_data and not "-" in specific_data['Disponibilidades'] else 0
+            newStock["receitaLiquida"] = float(specific_data['Receita Líquida'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Receita Líquida' in specific_data and not "-" in specific_data['Receita Líquida'] else 0
+            newStock["lucroLiquido"] = float(specific_data['Lucro Líquido'].replace('.', '').replace(',', '.').replace('%', '').replace("\n", "")) if 'Lucro Líquido' in specific_data and not "-" in specific_data['Lucro Líquido'] else 0
+
+            
+            final_stocks.append(newStock)
 
 
 
-    # Saves in mongoDB
-    client = MongoClient(os.environ['MONGO_URI'])
+        # Saves in mongoDB
+        client = MongoClient(os.environ['MONGO_URI'])
+
+        print("Peguei todos os stocks, agora preciso salvar")
 
 
-    recent_stocks_db = client.recentStocks
-    all_stocks_coll = recent_stocks_db.stocks
+        recent_stocks_db = client.recentStocks
+        all_stocks_coll = recent_stocks_db.stocks
 
-    # First drop the collection
-    recent_stocks_db.stocks.drop()
+        # First drop the collection
+        recent_stocks_db.stocks.drop()
 
-    for i in range(0, len(final_stocks)):
-        stock = final_stocks[i]
-        # Insert in mongo
-        stock_id = all_stocks_coll.insert_one(stock).inserted_id
-        print("Inserted object on recent Objects ", i + 1, " of ", len(final_stocks), " :",   stock_id)
+        for i in range(0, len(final_stocks)):
+            stock = final_stocks[i]
+            # Insert in mongo
+            stock_id = all_stocks_coll.insert_one(stock).inserted_id
+            print("Inserted object on recent Objects ", i + 1, " of ", len(final_stocks), " :",   stock_id)
+
+
+        # Send email of success
+        message = Mail(from_email='danilo_aleixo@hotmail.com', to_emails=os.environ.get('EMAIL_TO'), subject='Cron de analise diária, rodou certinho', html_content='Deu certo')
+        send_email(message)
+
+except Exception as e:
+    print("Deu erro", str(e))
+    # Send email of error
+    message = Mail(from_email='danilo_aleixo@hotmail.com', to_emails=os.environ.get('EMAIL_TO'), subject='ERROR: Cron de analise diária, deu merda', html_content='Deu errado em ' + str(e))
+    send_email(message)
+
+
+
